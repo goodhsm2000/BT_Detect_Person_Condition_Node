8658e9df69487289cedd5122f774690804a9d27f
lionhong <73740089+goodhsm2000@users.noreply.github.com>
2023-05-21 23:05:01 +0900

    Update detect_person_condition.cpp

--- src/detect_person_condition.cpp
+++ src/detect_person_condition.cpp
@@ -1,5 +1,3 @@
-// made by HSM
-
 #include <string>
 
 #include "nav2_behavior_tree/plugins/condition/detect_person_condition.hpp"
@@ -12,8 +10,8 @@ DetectPersonCondition::DetectPersonCondition(
   const BT::NodeConfiguration & conf)
 : BT::ConditionNode(condition_name, conf)
 {
-  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
-  subscription_ = node_->create_subscription<vision_msgs::msg::Detection2DArray>(
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node"); // Behavior Tree의 구성(Config)인 블랙보드에서 "node"라는 키(Key)에 해당하는 데이터를 가져음
+  subscription_ = node_->create_subscription<vision_msgs::msg::Detection2DArray>( // "/yolo/detections" topic의 메세지가 도착할 때마다 detectionCallback 함수를 실행시키는 subscriber
     "/yolo/detections",
     rclcpp::SystemDefaultsQoS(),
     std::bind(&DetectPersonCondition::detectionCallback, this, std::placeholders::_1));
@@ -24,19 +22,22 @@ BT::NodeStatus DetectPersonCondition::tick()
   std::lock_guard<std::mutex> lock(mutex_);
   if (person_detected_) 
   {
-    //person_detected_ = false;
     RCLCPP_INFO(node_->get_logger(), "Person detected");
     return BT::NodeStatus::FAILURE;
   } 
   else
+  {
     RCLCPP_INFO(node_->get_logger(), "Person didn't detected"); 
     return BT::NodeStatus::SUCCESS;
+  }
 }
 
 void DetectPersonCondition::detectionCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  for (const auto& detection : msg->detections) {
+  if (msg->detections.empty())
+    person_detected_ = false;
+  for (const auto& detection : msg->detections) { // 아무런 객체도 검출되지 않으면 이 코드조차 실행이 안됨
     for (const auto& hypothesis : detection.results) {
       if (hypothesis.id == "person") 
         person_detected_ = true;
@@ -54,3 +55,4 @@ BT_REGISTER_NODES(factory)
 {
   factory.registerNodeType<nav2_behavior_tree::DetectPersonCondition>("DetectPerson");
 }
+
